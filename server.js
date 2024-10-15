99700060b74a4981dad6eecd2c8971037c2407c6
Philipp Timmalog <ptimmalog@gmail.com>
2024-10-15 08:35:09 +0200

    initial commit

--- server.js
+++ server.js
@@ -77,6 +77,8 @@ function extractIssueDataFromPayload(payload) {
 app.post("/github-webhook", async (req, res) => {
   const event = req.headers["x-github-event"];
   const payload = req.body;
+  console.log("----- POST received");
+  console.log(req);
 
   if (event === "projects_v2_item" && payload) {
     const issue = extractIssueDataFromPayload(payload);
